24ae72864c8a1b11b73c694c33850b9a0a6bcc66
Sean Yang <seanyang0813@gmail.com>
2020-10-11 11:04:36 -0400

    added parsing the zoom url and new favicon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col flex justify-center w-1/3">
+  <div class="flex flex-col flex justify-center w-1/2 md:w-1/3">
     <div class="mb-6">
       <h1 class="font-sans Roboto font-bold text-4xl text-blue-500">Back of Zoom</h1>
     </div>
@@ -28,8 +28,26 @@ export default {
   },
   methods: {
     join: function () {
-      console.log(this.name)
-      console.log(this.id)
+      let name = this.name
+      let id = this.parseId()
+      console.log(name)
+      console.log(id)
+    },
+    parseId: function () {
+      //first check if it's the meeting id which is all digits
+      let pattern = /^\d+$/g
+      let result = this.id.match(pattern)
+      if (result != null) {
+        return parseInt(result[0])
+      }
+      //check if it's a valid zoom url
+      pattern = /zoom.us\/j\/(\d+)/
+      result = this.id.match(pattern)
+      
+      if (result != null) {
+        return parseInt(result[1])
+      }
+      return null
     }
   }
 }
