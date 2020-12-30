6f844846b189cc30dfa6897345d65b461e09ae31
Sean Yang <seanyang0813@gmail.com>
2020-12-29 20:38:22 -0500

    updated traffic tracking

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import {routes} from './route'
 import "./assets/css/tailwind.css"
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -10,6 +11,11 @@ const router = new VueRouter({
   mode: "history",
   routes
 })
+
+Vue.use(VueGtag, {
+  config: { id: "UA-186205576-1" }
+});
+
 
 new Vue({
   router,
